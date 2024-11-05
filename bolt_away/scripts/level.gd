b272a88a7af83c8abcbeb7ba2b14d6784ffe65b8
lapisspider <68882441+lapisspider@users.noreply.github.com>
2024-11-05 17:59:39 -0600

    Updated algorithm

---
+++ bolt_away/scripts/level.gd
@@ -0,0 +1,40 @@
+extends Node2D
+
+@export var chunks: Array[PackedScene] = []
+var startingChunk: PackedScene = preload("res://chunks/starting_chunk.tscn")
+
+var amnt = 5
+var offset = 512
+var start_offset = -200
+var rng = RandomNumberGenerator.new()
+
+func _ready() -> void:
+	MultiplayerManager.connect("multiplayer_mode_changed", _on_multiplayer_mode_changed)
+	MultiplayerManager.connect("player_joined", player_joined)
+	spawn_starting_chunks()
+
+func singleplayer_algorithm(n):
+	var num = RandomNumberGenerator.new().randi_range(0, chunks.size()-1)
+	add_chunk(num, n)
+
+func multiplayer_algorithm(n):
+	var num = rng.randi_range(0, chunks.size()-1)
+	add_chunk(num, n)
+
+func spawn_starting_chunks():
+	for n in amnt:
+		var instance = startingChunk.instantiate()
+		instance.position.x = n*offset + start_offset
+		add_child(instance)
+
+func add_chunk(num, chunkPosition):
+	var instance = chunks[num].instantiate()
+	instance.position.x = chunkPosition
+	add_child(instance)
+
+func _on_multiplayer_mode_changed(_multiplayer_enabled: bool):
+	rng.seed = MultiplayerManager.map_seed
+
+func player_joined(_multiplayer_enabled: bool):
+	if !multiplayer.is_server():
+		rng.seed = MultiplayerManager.map_seed
---
+++ bolt_away/scripts/multiplayer/multiplayer_manager.gd
@@ -0,0 +1,140 @@
+extends Node
+
+const SERVER_PORT = 8080
+const SERVER_IP = "127.0.0.1"
+
+var multiplayer_scene = preload("res://scenes/multiplayer_player.tscn")
+var _players_spawn_node 
+
+
+var host_mode_enabled = false	
+var multiplayer_mode_enabled = false
+var respawn_point = Vector2(30, 20)
+var map_seed = 0
+var dead_player_id = 0
+
+signal multiplayer_mode_changed(_multiplayer_mode_enabled: bool)
+signal player_joined(_multiplayer_mode_enabled: bool)
+
+func host():
+	print("hosting")
+	
+	_players_spawn_node = get_tree().get_current_scene().get_node("Players")
+	multiplayer_mode_enabled = true
+	host_mode_enabled = true
+	
+	var server_peer = ENetMultiplayerPeer.new()
+	server_peer.create_server(SERVER_PORT)
+	
+	multiplayer.multiplayer_peer = server_peer
+	
+	multiplayer.peer_connected.connect(_connect_player)
+	multiplayer.peer_disconnected.connect(_disconnect_player)
+	
+	_end_singleplayer()
+
+	map_seed = RandomNumberGenerator.new().randi()
+	emit_signal("multiplayer_mode_changed", multiplayer_mode_enabled)
+	
+	if not OS.has_feature("dedicated_server"):
+		_connect_player(1)
+	
+func join():
+	print("joining")
+	
+	_players_spawn_node = get_tree().get_current_scene().get_node("Players")
+	multiplayer_mode_enabled = true
+	
+	var client_peer = ENetMultiplayerPeer.new()
+	client_peer.create_client(SERVER_IP, SERVER_PORT)
+	
+	multiplayer.server_disconnected.connect(_server_disconnected)
+
+	multiplayer.multiplayer_peer = client_peer
+	_end_singleplayer()
+
+func _connect_player(id: int):
+	print("Player %s joined the game." % id)
+	
+	if !multiplayer.is_server():
+		return
+		
+	var player_to_add = multiplayer_scene.instantiate()
+	player_to_add.player_id = id
+	player_to_add.name = str(id)
+
+	_players_spawn_node.add_child(player_to_add, true)
+	rpc("sync_map_seed", map_seed)
+
+
+func _disconnect_player(id: int):
+	print("Player %s left the game." % id)
+	
+	if not _players_spawn_node.has_node(str(id)):
+		return
+	_players_spawn_node.get_node(str(id)).queue_free()
+
+	clean_multiplayer_state()
+	
+	
+func _end_singleplayer():
+	print("ending singleplayer")
+	var player_to_remove = get_tree().get_current_scene().get_node("Player")
+	player_to_remove.queue_free()
+	
+func _end_game(losing_player_id):
+	if multiplayer.is_server():
+		dead_player_id = losing_player_id
+		rpc("sync_losing_player_id", dead_player_id)
+		_game_ended(dead_player_id)
+
+
+@rpc("any_peer")
+func sync_losing_player_id(losing_player_id: int):
+	dead_player_id = losing_player_id
+	_game_ended(dead_player_id)
+
+func _game_ended(losing_player_id: int):
+	if multiplayer.get_unique_id() == losing_player_id:
+		_show_winning_message("You lost!")
+	else:
+		_show_winning_message("You won!") 
+
+	get_tree().paused = true
+
+func _show_winning_message(message: String):
+	var end_screen = get_tree().get_current_scene().get_node("EndGameScreen")
+	var label = end_screen.get_node("Message")
+	label.text = message
+	end_screen.show()
+
+
+@rpc("any_peer")
+func sync_map_seed(mySeed: int):
+	map_seed = mySeed
+	if !multiplayer.is_server():
+		emit_signal("player_joined", multiplayer_mode_enabled)
+
+func _server_disconnected():
+	clean_multiplayer_state()
+
+func clean_multiplayer_state():
+	#Disconnect signals
+	if multiplayer.peer_connected.is_connected(_connect_player):
+		multiplayer.peer_connected.disconnect(_connect_player)
+
+	if multiplayer.peer_disconnected.is_connected(_disconnect_player):
+		multiplayer.peer_disconnected.disconnect(_disconnect_player)
+
+	if multiplayer.server_disconnected.is_connected(_server_disconnected):
+		multiplayer.server_disconnected.disconnect(_server_disconnected)
+
+	# Close connection
+	if multiplayer.multiplayer_peer:
+		multiplayer.multiplayer_peer.close()
+		multiplayer.multiplayer_peer = OfflineMultiplayerPeer.new()
+
+	multiplayer_mode_enabled = false
+	host_mode_enabled = false
+	map_seed = 0
+	dead_player_id = 0
