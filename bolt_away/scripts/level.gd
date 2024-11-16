68eed385c28f38d1ca4c31aa5a0a879673ac4b2b
TheJackedUpGamer <101526570+TheJackedUpGamer@users.noreply.github.com>
2024-11-16 01:20:42 -0600

    Adding bolt_away
    This was the bolt_away file in main that was just before it got deleted

---
+++ bolt_away/scripts/level.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+
+@export var chunks: Array[PackedScene] = []
+var startingChunk: PackedScene = preload("res://chunks/starting_chunk.tscn")
+var deathWall: PackedScene = preload("res://scenes/death_wall.tscn")
+
+var amnt = 3
+var offset = 256
+var start_offset = -200
+var rng = RandomNumberGenerator.new()
+
+func _ready() -> void:
+	MultiplayerManager.connect("host_joined", set_map_seed)
+	MultiplayerManager.connect("client_joined", set_map_seed)
+	spawn_starting_chunks()
+
+func singleplayer_algorithm(n):
+	var num = RandomNumberGenerator.new().randi_range(0, chunks.size()-1)
+	add_chunk(num, n)
+
+func multiplayer_algorithm(n):
+	var num = rng.randi_range(0, chunks.size()-1)
+	add_chunk(num, n)
+
+func spawn_starting_chunks():
+	for n in amnt:
+		var instance = startingChunk.instantiate()
+		instance.position.x = n*offset + start_offset
+		add_child(instance)
+
+func add_chunk(num, chunkPosition):
+	var instance = chunks[num].instantiate()
+	instance.position.x = chunkPosition
+	add_child(instance)
+
+func set_map_seed():
+	rng.seed = MultiplayerManager.map_seed
---
+++ bolt_away/scripts/powerup_manager.gd
@@ -0,0 +1,57 @@
+extends Node
+
+enum PowerUpType {
+	NONE,
+	DASH,
+	JETPACK,
+	OILSPILL,
+	GRAPPLINGHOOK,
+}
+
+var current_powerup: PowerUpType = PowerUpType.NONE
+var jetpack_fuel = 100
+var is_jetpack_active = false
+@onready var oilspill_scene = preload("res://scenes/powerups/oilspill.tscn")
+@onready var parent = get_parent()
+@onready var PowerUpHUD = parent.get_node("PowerUpHUD")
+
+func collect_powerup(powerup: PowerUpType) -> void:
+	if current_powerup == PowerUpType.NONE:
+		current_powerup = powerup
+	PowerUpHUD.update_powerup_icon(powerup)
+
+func use_powerup() -> void:
+	match current_powerup:
+		PowerUpType.DASH:
+			print("Dash!")
+			parent.start_dash()
+		PowerUpType.JETPACK:
+			print("Jetpack!")
+			activate_jetpack()
+		PowerUpType.OILSPILL:
+			print("Oil spill!")
+			throw_oil()
+		PowerUpType.GRAPPLINGHOOK:
+			print("Grappling hook!")
+			parent.fire_grappling_hook()
+		PowerUpType.NONE:
+			print("No powerup!")
+	current_powerup = PowerUpType.NONE
+	if not is_jetpack_active:
+		PowerUpHUD.update_powerup_icon(PowerUpType.NONE)
+
+
+func activate_jetpack() -> void:
+	jetpack_fuel = 100
+	is_jetpack_active = true
+
+func deactivate_jetpack() -> void:
+	is_jetpack_active = false
+	PowerUpHUD.update_powerup_icon(PowerUpType.NONE)
+
+func throw_oil() -> void:
+	var oilspill = oilspill_scene.instantiate()
+	oilspill.position = parent.global_position + Vector2(64, 0)
+	get_tree().get_root().add_child(oilspill)
+	if MultiplayerManager.multiplayer_mode_enabled:
+		MultiplayerManager.rpc("spawn_oilspill", oilspill.position)
