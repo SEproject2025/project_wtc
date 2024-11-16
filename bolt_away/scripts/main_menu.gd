63f4b0ae4c63105be323e24c10d2bcf03f9bb80b
lapisspider <68882441+lapisspider@users.noreply.github.com>
2024-11-15 20:41:34 -0600

    Add files via upload

---
+++ bolt_away/scripts/main_menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+func _ready() -> void:
+	#Skip main menu if debugging or dedicated server
+	if OS.is_debug_build() or OS.has_feature("dedicated_server"):
+		call_deferred("_on_host_pressed")
+
+func _on_host_pressed():
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+func _on_join_pressed():
+	pass # Replace with function body.
+
+
+func _on_tutorial_pressed():
+	pass # Replace with function body.
+
+
+func _on_exit_pressed():
+	get_tree().quit()
