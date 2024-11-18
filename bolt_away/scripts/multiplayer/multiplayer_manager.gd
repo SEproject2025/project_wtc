6ac1912d6d3361df5859001ef71015dd219dae60
lapisspider <68882441+lapisspider@users.noreply.github.com>
2024-11-18 16:28:01 -0600

    movement, death wall, and chunk changes

--- bolt_away/scripts/multiplayer/multiplayer_manager.gd
+++ bolt_away/scripts/multiplayer/multiplayer_manager.gd
@@ -2,7 +2,7 @@ extends Node
 
 const SERVER_PORT = 8080
 const SERVER_IP = "127.0.0.1"
-const PLAYERS_TO_START_GAME = 1
+const PLAYERS_TO_START_GAME = 2
 const CENTER_OF_SPRITE = Vector2(3, -10) #Change if sprite changes
 
 var multiplayer_scene = preload("res://scenes/multiplayer_player.tscn")
