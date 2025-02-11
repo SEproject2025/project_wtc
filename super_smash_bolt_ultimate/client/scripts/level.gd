3d0ca509c290f6f29569c3aed2d7a4bb69f2175e
lapisspider <68882441+lapisspider@users.noreply.github.com>
2025-02-10 19:52:19 -0600

    chunk algorithm changes
    chunk algorithm changes modified to make addition biomes

--- super_smash_bolt_ultimate/client/scripts/level.gd
+++ super_smash_bolt_ultimate/client/scripts/level.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
-@export var chunks: Array[PackedScene]
+@export var chunks: Array[PackedScene] = []
+@export var forestchunks: Array[PackedScene] = []
+@export var labchunks: Array[PackedScene] = []
 var startingChunk: PackedScene = preload("res://chunks/starting_chunk.tscn")
 var deathWall: PackedScene = preload("res://scenes/death_wall.tscn")
 
 var amnt = 3
+var count = 1
 var offset = 512 
 var start_offset = -200
 var rng = RandomNumberGenerator.new()
@@ -14,8 +17,17 @@ func _ready() -> void:
 	spawn_starting_chunks()
 
 func algorithm(n):
-	var num = rng.randi_range(0, chunks.size()-1)
-	add_chunk(num, n)
+	if (count < 25):
+		var num = rng.randi_range(0, chunks.size()-1)
+		add_chunk(num, n)
+	elif (count < 50):
+		var num = rng.randi_range(0, forestchunks.size()-1)
+		add_chunk(num, n)
+	elif (count < 75):
+		var num = rng.randi_range(0, labchunks.size()-1)
+		add_chunk(num, n)
+	else:
+		count = 0
 
 func spawn_starting_chunks():
 	for n in amnt:
@@ -24,9 +36,21 @@ func spawn_starting_chunks():
 		add_child(instance)
 
 func add_chunk(num, chunkPosition):
-	var instance = chunks[num].instantiate()
-	instance.position.x = chunkPosition
-	add_child(instance)
+	
+	if (count < 25):
+		var instance = chunks[num].instantiate()
+		instance.position.x = chunkPosition
+		add_child(instance)
+	elif (count < 50):
+		var instance = forestchunks[num].instantiate()
+		instance.position.x = chunkPosition
+		add_child(instance)
+	elif (count < 75):
+		var instance = labchunks[num].instantiate()
+		instance.position.x = chunkPosition
+		add_child(instance)
+	count = count + 1
+	print(count)
 
 func set_map_seed(map_seed: int):	
 	rng.seed = map_seed
