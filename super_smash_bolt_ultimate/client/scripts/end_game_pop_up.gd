64339116c3be7d5db0c4f876d0f2101a409eea63
stevennarvaez1 <stevennarvaez35@gmail.com>
2025-03-07 19:21:34 -0600

    import

--- super_smash_bolt_ultimate/client/scripts/end_game_pop_up.gd
+++ super_smash_bolt_ultimate/client/scripts/end_game_pop_up.gd
@@ -1,7 +1,14 @@
 extends CanvasLayer
 
 var pop_up_template = preload("res://scenes/pop_up.tscn")
+var spectator_overlay = preload("res://scenes/spectator_overlay.tscn")
+var other_players = []
 
+func _ready() -> void:
+	other_players = get_tree().get_nodes_in_group("Players").filter(func(player): return player.name != str(User.ID) && player.alive)
+	if other_players.size() == 0:
+		$VBoxContainer/BoxContainer/Spectate.hide()
+	
 
 func _on_play_again_pressed():
 	var pop_up = pop_up_template.instantiate()
@@ -15,9 +22,9 @@ func _on_play_again_pressed():
 	User.client.send_left_game(User.current_lobby_name)
 	await get_tree().create_timer(1).timeout
 	User.client.request_lobby_list()
-	get_parent().get_parent().get_parent().add_child(load("res://scenes/lobby_menu.tscn").instantiate())
+	get_tree().get_root().add_child(load("res://scenes/lobby_menu.tscn").instantiate())
 	pop_up.queue_free()
-	var game_scene_node = get_node("../../../game_scene")
+	var game_scene_node = get_tree().get_root().get_node("game_scene")
 	game_scene_node.queue_free()
 	game_scene_node.get_node(str(User.ID)).queue_free()
 	queue_free()
@@ -29,3 +36,4 @@ func _on_spectate_pressed() -> void:
 	spectatorOverlay.spectate_player(0)
 	get_tree().get_root().add_child(spectatorOverlay)
 	queue_free()
+	
