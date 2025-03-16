44452601a5ff1a5eb84b1ce4c8e0c53d89eddbde
Beaulicious <beau.basden@students.pcci.edu>
2025-03-16 13:37:42 -0500

    Split Fuel HUD
    Upon activating a powerup that uses fuel, the UI displays an extra battery to show how much fuel is left for the powerup, and the baseplate of the UI is extended. Normal dash fuel is still displayed in its regular battery. Once the powerup fuel source is depleted, the extra battery and extended UI base disappear.

--- super_smash_bolt_ultimate/client/scripts/ui.gd
+++ super_smash_bolt_ultimate/client/scripts/ui.gd
@@ -2,13 +2,10 @@ extends CanvasLayer
 var fuel
 var power_fuel
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	fuel = get_node("boost_fuel")
 	power_fuel = get_node("power_fuel")
-	pass 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if power_fuel.value > 0:
 		$ui_base_extension.set_visible(true)
@@ -16,4 +13,3 @@ func _process(delta):
 	else:
 		$ui_base_extension.set_visible(false)
 		$powerup_battery.set_visible(false)
-	#pass
