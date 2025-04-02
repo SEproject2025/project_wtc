47ede72de8edacedb29e25cc8839f1008a0633d5
lapisspider <68882441+lapisspider@users.noreply.github.com>
2025-04-01 19:25:20 -0500

    Update death_wall.gd
    deathwall was too close

--- super_smash_bolt_ultimate/client/scripts/death_wall.gd
+++ super_smash_bolt_ultimate/client/scripts/death_wall.gd
@@ -3,7 +3,7 @@ extends Node2D
 const WALL_TERMINAL_VELOCITY := 150.0
 const WALL_INITIAL_VELOCITY  := 70.0
 const WALL_ACCELERATION      := 0.002
-const WALL_MAX_DISTANCE      := 400
+const WALL_MAX_DISTANCE      := 500
 
 var death_wall_start = false
 var   wall_velocity          = WALL_INITIAL_VELOCITY
