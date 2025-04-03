7a635b4aae8c28b65dc48212b6a5ef1ff558c515
TheJackedUpGamer <101526570+TheJackedUpGamer@users.noreply.github.com>
2025-04-03 03:59:24 -0500

    Main Menu Screen Implemented

--- super_smash_bolt_ultimate/client/scripts/main_menu.gd
+++ super_smash_bolt_ultimate/client/scripts/main_menu.gd
@@ -9,38 +9,6 @@ var lobby_menu
 func _ready() -> void:
 	name = "main_menu"
 
-func _on_play_pressed():
-	if get_child_count() > 5:
-		return
-	if control_flag:
-		return
-	
-	var user_text = $VBoxContainer/Username.text
-	
-	if user_text == "" or user_text.contains(" "):
-		var pop_up = pop_up_template.instantiate()
-		pop_up.set_msg("Enter a name!\nNo spaces are allowed!")
-		add_child(pop_up)
-		return
-	else:
-		if User.client:
-			User.client.queue_free()
-		User.client = Client.new()
-		get_parent().add_child(User.client)
-		control_flag = true
-		User.client.user_name_feedback_received.connect(go_to_lobby_menu)
-		User.client.invalid_user_name.connect(invalid_name)
-		$Loading.show()
-		await get_tree().create_timer(2).timeout
-		
-		if User.client.is_connection_valid():
-			lobby_menu = lobby_menu_template.instantiate()
-			User.client.request_lobby_list()
-			User.client.send_user_name($VBoxContainer/Username.text)
-	
-	
-	check_if_connected()
-
 func check_if_connected():
 	await get_tree().create_timer(2).timeout
 	
@@ -66,8 +34,8 @@ func go_to_lobby_menu():
 	get_parent().add_child(lobby_menu)
 	queue_free()
 
-func _on_username_text_submitted(_new_text):
-	_on_play_pressed()
+func _on_username_text_submitted(_new_text:):
+	_on_multiplayer_pressed()
 
 func _on_quit_pressed():
 	get_tree().quit(0)
@@ -80,3 +48,41 @@ func invalid_name():
 	pop_up.set_msg("Invalid name!")
 	new_main_menu.add_child(pop_up)
 	queue_free()
+
+
+func _on_multiplayer_pressed():
+	if $Title_Screen/Username.is_visible():
+		if get_child_count() > 5:
+			return
+		if control_flag:
+			return
+		
+		var user_text = $Title_Screen/Username.text
+		
+		if user_text == "" or user_text.contains(" "):
+			var pop_up = pop_up_template.instantiate()
+			pop_up.set_msg("Enter a name!\nNo spaces are allowed!")
+			add_child(pop_up)
+			return
+		else:
+			if User.client:
+				User.client.queue_free()
+			User.client = Client.new()
+			get_parent().add_child(User.client)
+			control_flag = true
+			User.client.user_name_feedback_received.connect(go_to_lobby_menu)
+			User.client.invalid_user_name.connect(invalid_name)
+			$Loading.show()
+			await get_tree().create_timer(2).timeout
+			
+			if User.client.is_connection_valid():
+				lobby_menu = lobby_menu_template.instantiate()
+				User.client.request_lobby_list()
+				User.client.send_user_name($Title_Screen/Username.text)
+		
+		check_if_connected()
+	else:
+		$Title_Screen/Username.set_visible(true)
+		await get_tree().create_timer(0.02).timeout
+		$Title_Screen/Title_Screen_box/Multiplayer10.set_visible(false)
+	pass # Replace with function body.
