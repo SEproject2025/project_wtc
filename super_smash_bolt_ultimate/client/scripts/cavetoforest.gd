e5ecee71012562393b4efe464b24c248083cfe6a
lapisspider <68882441+lapisspider@users.noreply.github.com>
2025-04-04 15:04:38 -0500

    oops
    forgot these changes

--- super_smash_bolt_ultimate/client/scripts/cavetoforest.gd
+++ super_smash_bolt_ultimate/client/scripts/cavetoforest.gd
@@ -12,13 +12,14 @@ const NUMBER_OF_TRAILING_HALF_CHUNKS = NUMBER_OF_TRAILING_CHUNKS * 2
 
 var gave_birth = false
 var player_in_cave = true
+var biome_switch_distance = 400
 
 func _physics_process(_delta):
 	var players = get_tree().get_nodes_in_group("Players")
 	var first_place = 0
 	var last_place  = 100000
 	for player in players:
-		if global_position.x - player.global_position.x < 400 and player.is_in_cave: #.distance_to(player.global_position) < 500 and player.global_position.x < global_position.x and player_in_cave:
+		if global_position.x - player.global_position.x < biome_switch_distance and player.is_in_cave: #.distance_to(player.global_position) < 500 and player.global_position.x < global_position.x and player_in_cave:
 			player.set_background("forest")
 			Cavesound.fading = true
 			Forestsound.play_sound = true
@@ -29,10 +30,10 @@ func _physics_process(_delta):
 					last_place = player.position.x
 			if(player.position.x > first_place):
 				first_place = player.position.x
-	if global_position.x - first_place <= 1000 and first_place < global_position.x and !gave_birth:
+	if global_position.x - first_place <= (level.offset*2) and first_place < global_position.x and !gave_birth:
 		level.algorithm(position.x+(level.offset))
 		gave_birth = true
-	if  last_place - global_position.x >= 1000 and last_place > global_position.x  and gave_birth:
+	if  last_place - global_position.x >= (level.offset*2) and last_place > global_position.x  and gave_birth:
 		queue_free()
 
 #func _physics_process(_delta):
--- super_smash_bolt_ultimate/client/scripts/foresttocave.gd
+++ super_smash_bolt_ultimate/client/scripts/foresttocave.gd
@@ -5,7 +5,7 @@ extends Node2D
 @onready var Cavesound = $"../../CaveAmbience"
 
 var gave_birth = false
-var anim_count = 0
+var biome_switch_distance = 400
 # func _ready() -> void:
 # 	User.client.some_one_left_game.connect(get_players)
 
@@ -17,19 +17,19 @@ func _physics_process(_delta):
 	var first_place = 0
 	var last_place  = 100000
 	for player in players:
-		if global_position.distance_to(player.global_position) > 400 and player.global_position.x > global_position.x and anim_count < 1:
+		if global_position.distance_to(player.global_position) > biome_switch_distance and player.global_position.x > global_position.x and !player.is_in_cave:
 			player.set_background("cave")
 			Forestsound.fading = true
 			Cavesound.play_sound = true
-			anim_count += 1
+			player.is_in_cave = true
 		if player.alive:
 			if (player.position.x < last_place):
 					last_place = player.position.x
 			if(player.position.x > first_place):
 				first_place = player.position.x
-	if global_position.x - first_place <= 1000 and first_place < global_position.x and !gave_birth:
+	if global_position.x - first_place <= (level.offset*2) and first_place < global_position.x and !gave_birth:
 		
 		level.algorithm(position.x+(level.offset))
 		gave_birth = true
-	if  last_place - global_position.x >= 1000 and last_place > global_position.x  and gave_birth:
+	if  last_place - global_position.x >= (level.offset*2) and last_place > global_position.x  and gave_birth:
 		queue_free()
