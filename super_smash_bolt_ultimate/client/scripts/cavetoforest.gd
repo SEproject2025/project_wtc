5cbc9bb3f2a6644ec040b81682437f3e383151c6
lapisspider <68882441+lapisspider@users.noreply.github.com>
2025-04-23 11:27:12 -0500

    fixed the chunk issues
    this fixed the chunk issue finally

--- super_smash_bolt_ultimate/client/scripts/cavetoforest.gd
+++ super_smash_bolt_ultimate/client/scripts/cavetoforest.gd
@@ -5,26 +5,33 @@ extends Node2D
 @onready var parallax_background = $"../../CaveBackground"
 @onready var Forestsound = $"../../ForestAmbience"
 @onready var Cavesound = $"../../CaveAmbience"
-
-var anim_count = 0
-var played = false
+var gave_birth = false
+var player_in_cave = true
+var biome_switch_distance = 400
 # func _ready() -> void:
 # 	User.client.some_one_left_game.connect(get_players)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta):
-	# if !players:
-	# 	get_players()
 	var players = get_tree().get_nodes_in_group("Players")
+				
+	var first_place = 0
+	var last_place  = 100000
 	for player in players:
-		if (global_position.distance_to(player.global_position) < 500 and player.global_position.x < global_position.x and anim_count != 1)\
-			or (level.count >= 5 and level.count < 10 and not played):
-			parallax_background.cavetoforest()
-			Cavesound.fading = true
-			Forestsound.play_sound = true
+		if player.alive:
+			if global_position.x - player.global_position.x < biome_switch_distance and player_in_cave: #.distance_to(player.global_position) < 500 and player.global_position.x < global_position.x and player_in_cave:
+				parallax_background.cavetoforest()
+				Cavesound.fading = true
+				Forestsound.play_sound = true
+				player_in_cave = false
+				print("animation instantiated!")
+			if (player.position.x < last_place):
+					last_place = player.position.x
+			if(player.position.x > first_place):
+				first_place = player.position.x
+		if global_position.x - first_place <= (level.offset*2) and first_place < global_position.x and !gave_birth:
 			
-			anim_count += 1
-			played = true
-		if global_position.distance_to(player.global_position) > 1000 and player.global_position.x > global_position.x:
-			level.algorithm(position.x+(level.amnt*level.offset))
+			level.algorithm(position.x+(level.offset))
+			gave_birth = true
+		if  last_place - global_position.x >= (level.offset*2) and last_place > global_position.x  and gave_birth:
 			queue_free()
--- super_smash_bolt_ultimate/client/scripts/death_wall.gd
+++ super_smash_bolt_ultimate/client/scripts/death_wall.gd
@@ -22,12 +22,3 @@ func _process(delta: float) -> void:
 			if(wall_velocity <= WALL_TERMINAL_VELOCITY):
 				wall_velocity += WALL_ACCELERATION
 		position.x += (wall_velocity * delta)
-
-func singleplayer_algorithm():
-	pass
-	var player = get_parent().get_node("Player")
-	if((player.position.x - position.x ) > WALL_MAX_DISTANCE):
-		position.x += (player.position.x - position.x) - WALL_MAX_DISTANCE
-	else:
-		if (wall_velocity <= WALL_TERMINAL_VELOCITY):
-			wall_velocity += WALL_ACCELERATION
